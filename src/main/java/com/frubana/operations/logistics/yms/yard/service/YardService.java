3ae7a7026634bf15cdfee8cc215a86b0ded31b5d
sebastian hidalgo <sebashidalgo1995@gmail.com>
2021-04-15 01:14:58 -0500

    Revert "Merge pull request #3 from papugarces/refactor_free_yard"
    This reverts commit 96caf61af8c30903f05ecb9cb9140253a676740a, reversing
    changes made to e6d4ae0213f51bf216d3cf128dc16cc03542022b.

--- src/main/java/com/frubana/operations/logistics/yms/yard/service/YardService.java
+++ src/main/java/com/frubana/operations/logistics/yms/yard/service/YardService.java
@@ -99,7 +99,7 @@ public class YardService implements HealthCheck {
     public Yard liberar(Yard yard) {
        Yard yardFound=getByWarehouseAndAssignationNumber(yard.getWarehouse(), yard.getAssignationNumber());
        if(yardFound!=null){
-        return this.repository.updateColorYard(yard);
+        return this.repository.modificar(yard);
        }else{
            return null;
        }
--- src/main/resources/migrations/V2021.04.14.10.45.01__add_default_color.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE public.yard ADD default_color varchar;
